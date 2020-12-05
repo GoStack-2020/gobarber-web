1e35b70f4807b2fbc6dfb45fb3ab7e140b5f16e9
Indiara Duarte <indiara96d@gmail.com>
2020-12-05 20:53:00 -0300

    Creating api mock and test set user data into local storage

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from '../../hooks/auth';
+import MockAdapter from 'axios-mock-adapter';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
 
 describe('Auth hook', () => {
-  it('should be to sign in', () => {
-    const { result } = renderHook(() => useAuth(), {
+  it('should be to sign in', async () => {
+    const apiResponse = {
+      user: {
+        id: 'user123',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+      token: 'token123',
+    };
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
@@ -13,6 +29,10 @@ describe('Auth hook', () => {
       password: '123456',
     });
 
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:token', apiResponse.token);
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:user', JSON.stringify(apiResponse.user));
     expect(result.current.user.email).toEqual('johndoe@example.com');
   });
 });
